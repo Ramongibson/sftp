7d0ed551567e89faae0a6ce46dad5093ef2d76f0
Ramon Gibson <Ramongibson23@gmail.com>
2018-01-28 23:27:43 -0600

    fix

--- src/main/java/com/ramongibson/Main.java
+++ src/main/java/com/ramongibson/Main.java
@@ -3,7 +3,7 @@ package com.ramongibson;
 public class Main {
 
 	public static void main(String[] args) {
-		SFTPManager sftpManager = new SFTPManager("192.168.1.11", "Ray", "4933");
+		SFTPManager sftpManager = new SFTPManager("ip", "user", "pass");
 		System.out.println(sftpManager.connect());
 		sftpManager.sendFile("/users/user/downloads/Elastic.pdf", "/users/user/downloads/new");
 	}
